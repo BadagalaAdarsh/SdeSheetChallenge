94537db0cee4373ed062886dfe0d2e4315075fe3
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-06-13 23:26:48 +0530

    Create Remove_Duplicates_From_Sorted_Array.cpp

---
+++ Day_9/Remove_Duplicates_From_Sorted_Array.cpp
@@ -0,0 +1,13 @@
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+    
+    int count = 0;
+    
+    for(int i = 1; i < n; i++) {
+        if(arr[i] == arr[i - 1]) count++;
+        else arr[i - count] = arr[i];
+    }
+    
+    return n - count;
+ 
+}
