3a96facc780ccf5822dcaa6f018a072b06538697
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-06-14 17:06:51 +0530

    Create Job_Sequencing_Problem.cpp

---
+++ Day_10/Job_Sequencing_Problem.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+int jobScheduling(vector<vector<int>> &jobs)
+{
+    // Write your code here
+    sort(jobs.begin(), jobs.end());
+    int n = jobs.size();
+    
+    vector<int> dp(n+1, 0);
+    vector<int> curr(n+1, 0);
+    
+    for(int i = n - 1; i >= 0; i--) {
+        for(int t = n - 1; t >= 0; t--) {
+            int skip = dp[t];
+            int done = 0;
+            if(jobs[i][0]-t > 0) done = jobs[i][1] + dp[t + 1];
+            curr[t] = max(skip, done);
+        }
+        dp = curr;
+    }
+    
+    return dp[0];
+}
