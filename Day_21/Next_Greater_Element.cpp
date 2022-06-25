f59c85aee0c211ba1163ff49873bc331e6de03f3
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-06-25 23:49:52 +0530

    Create Next_Greater_Element.cpp

---
+++ Day_21/Next_Greater_Element.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Gourav Yo
+ vector < int > nextGreater(vector < int > & nums,int n) {
+//       int n = nums.size();
+      vector < int > nge(n, -1);
+      stack < int > st;
+      for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= nums[i ]) {
+          st.pop();
+        }
+
+        if (i < n) {
+          if (!st.empty()) nge[i] = st.top();
+        }
+        st.push(nums[i ]);
+      }
+      return nge;
+    
+};
