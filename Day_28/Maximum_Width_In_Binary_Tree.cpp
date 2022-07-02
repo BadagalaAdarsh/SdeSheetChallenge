68131af8047ea1485372c5f7354deb6bc6021551
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-07-02 23:47:55 +0530

    Create Maximum_Width_In_Binary_Tree.cpp

---
+++ Day_28/Maximum_Width_In_Binary_Tree.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+int getMaxWidth(TreeNode<int> *root)
+{
+    // Write your code here.
+    if(root == NULL) return 0;
+    queue<TreeNode<int>*> q;
+    q.push(root);
+    int cnt = 0;
+    while(!q.empty()){
+        int n = q.size();
+        cnt = max(cnt, n);
+        
+        for(int i = 0; i < n; i++){
+            TreeNode<int> *tmp = q.front();
+            q.pop();
+            if(tmp->left) q.push(tmp->left);
+            if(tmp->right) q.push(tmp->right);
+        }
+    }
+    
+    return cnt;
+    
+}
