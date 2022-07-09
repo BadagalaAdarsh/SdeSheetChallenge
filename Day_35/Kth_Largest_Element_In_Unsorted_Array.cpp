2b77627cb4b59483c6bd9584802835321ac9b567
Adarsh Badagala <58242963+BadagalaAdarsh@users.noreply.github.com>
2022-07-09 20:39:49 +0530

    Create Kth_Largest_Element_In_Unsorted_Array.cpp

---
+++ Day_35/Kth_Largest_Element_In_Unsorted_Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+int kthLargest(vector<int>& arr, int size, int K)
+{
+    priority_queue<int,vector<int>,greater<int>> pq;
+        
+        for(int i = 0;i<K;i++)
+            pq.push(arr[i]);
+        
+        for(int i=K;i<size;i++)
+        {
+            if(arr[i]>pq.top())
+            {
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        
+        return pq.top();
+	// Write your code here.
+}
